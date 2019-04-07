d661054fb28d9f73970ac49da591547abb55d975
skye347 <s347419313@gmail.com>
2019-04-07 15:33:57 +0800

    impl future edition

---
+++ src/codec.rs
@@ -0,0 +1,47 @@
+use tokio::codec::{Encoder,Decoder};
+use bytes::BytesMut;
+use crate::request::Request;
+use crate::response::Response;
+use bytes::BigEndian;
+use std::io::Error as IoError;
+use std::io::ErrorKind;
+use std::u32;
+use bytes::BufMut;
+use bytes::ByteOrder;
+
+pub struct RequestCodec;
+pub struct ResponseCodec;
+
+impl Encoder for RequestCodec {
+    type Item = Request;
+    type Error = IoError;
+
+    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        let data=item.to_json().to_string();
+        let len = data.len();
+        if len > u32::max_value() as usize {
+            // to long
+            return Err(IoError::new(ErrorKind::InvalidInput,"len is too long (> u32::MAX)"))
+        }
+        dst.reserve(4+len);
+        dst.put_u32_be(len as u32);
+        dst.put(data);
+        Ok(())
+    }
+}
+
+impl Decoder for ResponseCodec {
+    type Item = (Response,String);
+    type Error = IoError;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        let len = BigEndian::read_u32(src.as_ref()) as usize;
+        if len+4 > src.len() {
+            return Ok(None);
+        }
+        let mut rest = src.split_to(4+len);
+        let rest= rest.split_off(4);
+        let result = Response::from_slice(rest.as_ref());
+        Ok(Some(result))
+    }
+}
---
+++ src/future.rs
@@ -0,0 +1,246 @@
+use std::cell::Cell;
+use std::collections::HashMap;
+use std::io::Error as IoError;
+use std::net::{IpAddr, SocketAddr};
+use std::sync::{Arc, RwLock};
+
+use crossbeam::sync::ArcCell;
+use futures::{AsyncSink, IntoFuture, StartSend};
+use futures::stream::Forward;
+use futures::sync::mpsc::*;
+use futures::sync::oneshot::{self, Receiver as OneshotReceiver, Sender as OneshotSender};
+use serde_json::Value;
+use tokio::codec::{FramedRead, FramedWrite};
+use tokio::io::{ErrorKind, ReadHalf, WriteHalf};
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::net::TcpStream;
+use tokio::prelude::{Async, Poll, Stream};
+use tokio::prelude::future::Future;
+use tokio::prelude::Sink;
+
+use crate::codec::{RequestCodec, ResponseCodec};
+use crate::future::Sender::Unbounded;
+use crate::request;
+use crate::request::Request;
+use crate::request::Request::register;
+use crate::response::Response;
+
+pub struct Eventbus {
+    tx: Arc<RwLock<HashMap<String, Sender>>>,
+    writer: UnboundedSender<Request>,
+}
+
+pub struct EventbusWriteStream {
+    inner: FramedWrite<WriteHalf<TcpStream>, RequestCodec>,
+    rx: UnboundedReceiverWithError<Request>
+}
+
+pub struct EventbusReadStream {
+    reader: FramedRead<ReadHalf<TcpStream>, ResponseCodec>,
+    tx: Arc<RwLock<HashMap<String, Sender>>>,
+}
+
+enum Sender {
+    Unbounded(UnboundedSender<Response>),
+    Oneshot(ArcCell<Option<OneshotSender<Response>>>),
+}
+
+impl Stream for EventbusReadStream {
+    type Item = (Response, String);
+    type Error = std::io::Error;
+
+    fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        match self.reader.poll() {
+            Ok(Async::Ready(Some((response, address)))) => {
+                self.send(&address,response.clone());
+                return Ok(Async::Ready(Some((response, address))));
+            }
+            other => other
+        }
+    }
+}
+
+pub struct UnboundedReceiverWithError<T>(pub UnboundedReceiver<T>);
+
+impl<T> Stream for UnboundedReceiverWithError<T> {
+    type Item = T;
+    type Error = IoError;
+
+    fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        match self.0.poll() {
+            Err(_)=>{
+                Err(IoError::new(ErrorKind::BrokenPipe,"UnboundedSender closed ?"))
+            }
+            Ok(other)=>{
+                Ok(other)
+            }
+        }
+    }
+}
+
+impl IntoFuture for EventbusWriteStream {
+    type Future = Forward<UnboundedReceiverWithError<Request>,FramedWrite<WriteHalf<TcpStream>,RequestCodec>>;
+    type Item = (UnboundedReceiverWithError<Request>,FramedWrite<WriteHalf<TcpStream>,RequestCodec>);
+    type Error = IoError;
+
+    fn into_future(self) -> Self::Future {
+        self.rx.forward(self.inner)
+    }
+}
+
+pub struct ResponseStream {
+    rx: UnboundedReceiver<Response>
+}
+
+pub struct ResponseFut {
+    rx: OneshotReceiver<Response>
+}
+
+impl Future for ResponseFut {
+    type Item = Response;
+    type Error = IoError;
+
+    fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
+        match self.rx.poll() {
+            Ok(i)=>Ok(i),
+            Err(e)=>Err(IoError::new(ErrorKind::Other,"canceled"))
+        }
+    }
+}
+
+impl Stream for ResponseStream {
+    type Item = Response;
+    //todo: Error
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        self.rx.poll().map_err(|_| ())
+    }
+}
+
+impl Eventbus {
+    pub fn connect(address: IpAddr, port: u16) -> impl Future<Item=(Eventbus,EventbusReadStream,EventbusWriteStream),Error=IoError> {
+        TcpStream::connect(&SocketAddr::new(address.clone(), port)).map(move |s| {
+            let (write_tx, write_rx) = unbounded();
+            let map = Arc::new(RwLock::new(HashMap::new()));
+            let (r, w) = s.split();
+            let reader = FramedRead::new(r, ResponseCodec);
+            let writer = FramedWrite::new(w, RequestCodec);
+            let read_stream = EventbusReadStream {
+                reader,
+                tx: map.clone(),
+            };
+            let write_stream = EventbusWriteStream {
+                inner: writer,
+                rx: UnboundedReceiverWithError(write_rx)
+            };
+            let eventbus = Eventbus {
+                tx: map,
+                writer: write_tx
+            };
+            (eventbus,read_stream,write_stream)
+        })
+    }
+
+    fn send_frame(&self, req:Request)->Result<(),SendError<Request>> {
+        self.writer.unbounded_send(req)
+    }
+
+    pub fn register(&self, address: String, headers: Option<Value>) -> Result<ResponseStream,SendError<Request>> {
+        let req = request::Request::register(request::RegisterObject{
+            address: address.to_string(),
+            headers,
+        });
+        self.send_frame(req).map(|_|{
+            let (tx, rx) = unbounded();
+            let mut map = self.tx.write().unwrap();
+            map.insert(address, Sender::Unbounded(tx));
+            ResponseStream {
+                rx
+            }
+        })
+    }
+
+    pub fn unregister(&self, address:String) {
+        let req=request::Request::unregister(request::RegisterObject{
+            address: address.to_string(),
+            headers: None
+        });
+        self.send_frame(req).unwrap();
+        let mut map=self.tx.write().unwrap();
+        map.remove(&address);
+    }
+
+    pub fn ping(&mut self)->Result<(),SendError<Request>>{
+        let s=request::Request::ping;
+        self.send_frame(s)
+    }
+
+    pub fn send(&self,address:String,message:Value)->Result<(),SendError<Request>> {
+        let req=request::Request::send(request::RegularRequestObject{
+            address:address.to_string(),
+            body:message,
+            headers:None,
+            replyAddress:Some(address.to_string())
+        });
+        self.send_frame(req)
+    }
+
+    pub fn send_reply(&self,address:String, message:Value)->Result<ResponseFut,SendError<Request>> {
+        let req=request::Request::send(request::RegularRequestObject{
+            address:address.to_string(),
+            body:message,
+            headers:None,
+            replyAddress:Some(address.to_string())
+        });
+        self.send_frame(req).map(|_|{
+            let (tx, rx) = oneshot::channel();
+            let mut map = self.tx.write().unwrap();
+            map.insert(address, Sender::Oneshot(ArcCell::new(Arc::new(Some(tx)))));
+            ResponseFut {
+                rx
+            }
+        })
+    }
+
+    pub fn publish(&mut self,address:String,message:Value)->Result<(),SendError<Request>>{
+        let req=request::Request::publish(request::RegularRequestObject{
+            address:address.to_string(),
+            body:message,
+            headers:None,
+            replyAddress:None
+        });
+        self.send_frame(req)
+    }
+}
+
+impl EventbusReadStream {
+    fn send(&mut self, address: &String, response: Response) {
+        let remove = if let Some(tx) = self.tx.read().unwrap().get(address) {
+            match tx {
+                Sender::Unbounded(tx) => {
+                    match tx.unbounded_send(response) {
+                        Ok(_) => false,
+                        Err(_) => true
+                    }
+                }
+                Sender::Oneshot(cell) => {
+//                    if let Some(tx) = cell.set(None) {
+//                        tx.send(response);
+//                    };
+//                    true
+                    let tx_opt = Arc::try_unwrap(cell.set(Arc::new(None))).unwrap();
+                    if let Some(tx) = tx_opt {
+                        tx.send(response);
+                    }
+                    true
+                }
+            }
+        } else { false };
+        if remove {
+            if let Ok(mut map_mut) = self.tx.write() {
+                map_mut.remove(address);
+            }
+        }
+    }
+}
